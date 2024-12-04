eb2c28f0dc545c2b01a90cb0004c0b9cffcf6e7f
GustavojSaraiva <102226082+GustavojSaraiva@users.noreply.github.com>
2024-12-04 09:11:53 -0300

    Add files via upload

--- insert_dimencao.sql
+++ insert_dimencao.sql
@@ -1,15 +1,15 @@
 -- Inserir na tabela de dimensão orgao_pagador
-INSERT IGNORE INTO orgao_pagador (nomedoorgaopagador)
-SELECT DISTINCT nomedoorgaopagador
+INSERT IGNORE INTO orgao_pagador (nome_do_orgao_pagador)
+SELECT DISTINCT nome_do_orgao_pagador
 FROM pagamento2324;
 
 -- Inserir na tabela de dimensão orgao_solicitante
-INSERT IGNORE INTO orgao_solicitante (nomeorgaosolicitante)
-SELECT DISTINCT nomeorgaosolicitante
+INSERT IGNORE INTO orgao_solicitante (nome_orgao_solicitante)
+SELECT DISTINCT nome_orgao_solicitante
 FROM viagem2324;
 
 -- Inserir na tabela de dimensão pagamento
-INSERT IGNORE INTO pagamento (tipodepagamento)
-SELECT DISTINCT tipodepagamento
+INSERT IGNORE INTO pagamento (tipo_de_pagamento)
+SELECT DISTINCT tipo_de_pagamento
 FROM pagamento2324;
 
